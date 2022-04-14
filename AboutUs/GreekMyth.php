b41ff38f2ff26559d7f36a31c2a31fc8cd666bc3
kongtaemin <rhdxoals@gmail.com>
2022-04-14 09:28:24 +0900

    GreekMyth.php : 그리스 신화란? 페이지 완

--- AboutUs/GreekMyth.php
+++ AboutUs/GreekMyth.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="kor">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,9 +9,13 @@
     <link rel="stylesheet" href="../css/common.css">
     <link rel="stylesheet" href="../css/subpage_common.css">
     <link rel="stylesheet" href="../css/about.css">
+    <link rel="stylesheet" href="../css/sub/type.css">
+    <link rel="stylesheet" href="../css/templates.css">
+    <link rel="stylesheet" href="../css/content.css">
     <script src="//code.jquery.com/jquery-3.4.1.min.js"></script>
-  
+
 </head>
+
 <body>
     <div id="wrap">
         <header>
@@ -18,7 +23,7 @@
         </header>
 
         <section id="slider">
-           <img src="../img/aboutus/sub_visual_bg.jpg" alt="">
+            <img src="../img/aboutus/sub_visual_bg.jpg" alt="">
         </section>
         <div id="sub_main">
             <aside id="left_menu">
@@ -26,8 +31,30 @@
             </aside>
             <section id="sub_contents">
                 <h3>박물관 소개<span>HOME>그리스박물관>그리스신화란?</span></h3>
-                
-                <h1>그리스신화란?</h1>
+
+                <div id="contents">
+
+                    <!--내용시작-->
+                    <div id="txt_box_color" style="margin-bottom: 2em;">
+                        <div id="txt_box_color_wrap">
+                            <p class="ls5"><span class="title"><img src="/image/default/01/greek_title.gif"
+                                        alt="그리스신화란?"></span>
+                                <strong class="blue">그리스신화는 고대 그리스에서부터 내려오는 세대를 넘나드는 신들과 영웅들의 이야기를 모아 놓은 것입니다.</strong>
+                                <br>
+                                수 천 년이 지난 지금도 전 세계적으로 널리 읽히고 있는 것은 이야기 속에 교훈과 감동이 시대를 초월해서 담겨 있기 때문입니다. <br>
+                                또한 그리스신화를 통해 우리는 서구문화를 이해하고 철학과 심리학, 인문학적 화두를 들여다 볼 수 있습니다.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="con_pic02"><img src="/image/default/01/greek_pic.jpg" alt="박물관 관련 사진"></div>
+
+                    <h4 class="greek_title"><img src="/image/default/01/greek_title02.gif" alt="그리스신화를 알아야 하는 이유"></h4>
+                    <div class="con_pic02"><img src="/image/default/01/greek_img01.jpg" alt="그리스신화를 알아야 하는 이유"></div>
+
+                    <div class="greek_img02"><img src="/image/default/01/greek_img02.gif"
+                            alt="자! 이제 그리스신화를 통해 무한한 상상의 세계로 빠져 볼까요?"></div>
+                </div>
+                <!-- 내용 끝 -->
                 <article id="sub_contact">
                     <div>
                         <h5>관람문의<span>Tel : 064-773-5800</span>관람시간<span>09:00~18:00 (입장마감 17:00)</span></h5>
@@ -38,7 +65,8 @@
         <footer>
             <?php include("../footer.html"); ?>
         </footer>
- </div>
-    
+    </div>
+
 </body>
+
 </html>
