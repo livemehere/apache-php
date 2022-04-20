11dfaf5498de6835d6eba6f1a2d506597430d0ed
kongtaemin <rhdxoals@gmail.com>
2022-04-20 13:53:34 +0900

    동여상페이지 width값 수정

--- AboutUs/Provideo.php
+++ AboutUs/Provideo.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="kor">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,25 +10,103 @@
     <link rel="stylesheet" href="../css/subpage_common.css">
     <link rel="stylesheet" href="../css/about.css">
     <script src="//code.jquery.com/jquery-3.4.1.min.js"></script>
- 
+
 </head>
+
 <body>
     <div id="wrap">
         <header>
             <?php include("../header.html"); ?>
         </header>
-
         <section id="slider">
-           <img src="../img/aboutus/sub_visual_bg.jpg" alt="">
+            <img src="../img/aboutus/sub_visual_bg.jpg" alt="">
         </section>
         <div id="sub_main">
             <aside id="left_menu">
                 <?php include ("left_menu.html"); ?>
             </aside>
             <section id="sub_contents">
-                <h3>박물관 소개<span>HOME>그리스박물관>홍보동영상</span></h3>
-                
-                <h1>홍보 동영상</h1>
+                <div id="board_movie">
+                    <!-- view 시작 -->
+                    <div id="view">
+                        <div class="view_on">
+                            <iframe src="http://www.youtube.com/embed/ORSHkCL8YPI" frameborder="0"
+                                allowfullscreen=""></iframe>
+                        </div>
+                        <div>
+                            <div class="view_text">
+                                <h5 class="none">미리보기</h5>
+                                <div class="icon_new"></div>
+                                <div class="paragraph_box">
+                                    <p><a href="http://360map.co.kr/jejudo/greekmythology" target="_blank">
+                                            Greekmythology
+                                            Museum Preview</a></p>
+                                </div>
+                                <div class="text_blur"></div>
+                            </div>
+                            <div class="share">
+                                <h5>Share Movie</h5>
+                                <ul>
+                                    <li><a href="#" target="_blank" title="새창열림" class="none"><img
+                                                src="http://www.greekmythology.co.kr/default/Bd/skin/basic_movie/img/icon_share01_off.png"
+                                                alt="페이스북"></a></li>
+                                    <li><a href="#" target="_blank" title="새창열림" class="none"><img
+                                                src="http://www.greekmythology.co.kr/default/Bd/skin/basic_movie/img/icon_share02_off.png"
+                                                alt="트위터"></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- view 끝 -->
+                    <!-- list 시작 -->
+                    <ul id="list">
+                        <li>
+                            <div class="movie_frame_on">
+                                <dl>
+                                    <dt><a href="#">미리보기</a></dt>
+                                    <dd><a href="#"> Greekmythology
+                                            Museum Preview</a></dd>
+                                </dl>
+                            </div>
+                            <img src="http://www.greekmythology.co.kr/default/Bd/upload/media/HP%20Thum_1.jpg" alt="">
+                        </li>
+                        <li>
+                            <div class="icon_new"></div>
+                            <div class="movie_frame">
+                                <dl>
+                                    <dt><a href="#">소개 영상</a></dt>
+                                    <dd><a href="#">Greek Mythology
+                                            Museum PR Video</a></dd>
+                                </dl>
+                            </div>
+                            <img src="http://www.greekmythology.co.kr/default/Bd/upload/media/movie_pic02.jpg"
+                                alt="Demi Lovato">
+                        </li>
+                        <li class="end">
+                            <div class="icon_new"></div>
+                            <div class="movie_frame">
+                                <dl>
+                                    <dt><a href="#">미로 공원</a></dt>
+                                    <dd><a href="#">Maze Park</a></dd>
+                                </dl>
+                            </div>
+                            <img src="http://www.greekmythology.co.kr/default/Bd/upload/media/%ED%99%88%ED%8E%98%EC%9D%B4%EC%A7%80_2.jpg"
+                                alt="Maze Park">
+                        </li>
+                        <li>
+                            <div class="icon_new"></div>
+                            <div class="movie_frame">
+                                <dl>
+                                    <dt><a href="#">방송 출연</a></dt>
+                                    <dd><a href="#">Tooniverse
+                                            '막이래쇼'</a></dd>
+                                </dl>
+                            </div>
+                            <img src="http://www.greekmythology.co.kr/default/Bd/upload/media/movie_pic01.jpg" alt="">
+                        </li>
+                    </ul>
+                    <!-- list 끝 -->
+                </div>
                 <article id="sub_contact">
                     <div>
                         <h5>관람문의<span>Tel : 064-773-5800</span>관람시간<span>09:00~18:00 (입장마감 17:00)</span></h5>
@@ -38,7 +117,9 @@
         <footer>
             <?php include("../footer.html"); ?>
         </footer>
- </div>
-    
+    </div>
 </body>
+
+</body>
+
 </html>
